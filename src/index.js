10dab7133905dbd52b04a51a1ef53f2a84465628
Marcos Cury <marcospcury@gmail.com>
2018-09-07 13:04:06 -0300

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/timeline.css';
+import './css/reset.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
