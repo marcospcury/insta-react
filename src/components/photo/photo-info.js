4190c3ba2ddbf030e00627ae268f4933b4b65e11
Marcos Cury <marcospcury@gmail.com>
2018-09-07 20:38:24 -0300

    Adding photo comment feature

--- src/components/photo/photo-info.js
+++ src/components/photo/photo-info.js
@@ -5,7 +5,7 @@ import PubSub from 'pubsub-js';
 export default class PhotoInfo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { likers: this.props.photo.likers };
+    this.state = { likers: this.props.photo.likers, comments: this.props.photo.comentarios };
   }
 
   componentWillMount() {
@@ -22,6 +22,13 @@ export default class PhotoInfo extends React.Component {
           console.log(likersUpdate);
           this.setState({ likers: likersUpdate });
         }
+      }
+    });
+
+    PubSub.subscribe('new-comment', (topico, commentInfo) => {
+      if (this.props.photo.id === commentInfo.photoId) {
+        const newComments = this.state.comments.concat(commentInfo.newComment);
+        this.setState({ comments: newComments });
       }
     });
   }
@@ -42,7 +49,7 @@ export default class PhotoInfo extends React.Component {
           {this.props.photo.comentario}
         </p>
         <ul className="foto-info-comentarios">
-          {this.props.photo.comentarios.map(comment => {
+          {this.state.comments.map(comment => {
             return (
               <li className="comentario" key={comment.id}>
                 <Link to={`/profile/${comment.login}`} className="foto-info-autor">{comment.login} </Link>
--- src/components/photo/photo-updates.js
+++ src/components/photo/photo-updates.js
@@ -6,6 +6,7 @@ export default class PhotoUpdates extends React.Component {
     super(props);
     this.state = { liked: props.photo.likeada };
   }
+
   like(event) {
     event.preventDefault();
     fetch(`http://localhost:8080/api/fotos/${this.props.photo.id}/like?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, { method: 'POST' })
@@ -13,7 +14,7 @@ export default class PhotoUpdates extends React.Component {
         if (response.ok) {
           return response.json();
         } else {
-          throw new Error("não foi possível realizar o like da foto");
+          throw new Error("Não foi possível realizar o like da foto");
         }
       })
       .then(liker => {
@@ -22,12 +23,35 @@ export default class PhotoUpdates extends React.Component {
       });
   }
 
+  postComment(event) {
+    event.preventDefault();
+    const requestInfo = {
+      method: 'POST',
+      body: JSON.stringify({ texto: this.comment.value }),
+      headers: new Headers({
+        'Content-type': 'application/json'
+      })
+    };
+
+    fetch(`http://localhost:8080/api/fotos/${this.props.photo.id}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, requestInfo)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("não foi possível comentar");
+        }
+      })
+      .then(newComment => {
+        PubSub.publish('new-comment', { photoId: this.props.photo.id, newComment });
+      });
+  }
+
   render() {
     return (
       <section className="fotoAtualizacoes">
         <a onClick={this.like.bind(this)} className={this.state.liked ? 'fotoAtualizacoes-like-ativo' : 'fotoAtualizacoes-like'}>Like</a>
-        <form className="fotoAtualizacoes-form">
-          <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" />
+        <form className="fotoAtualizacoes-form" onSubmit={this.postComment.bind(this)}>
+          <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" ref={input => this.comment = input} />
           <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit" />
         </form>
       </section>
