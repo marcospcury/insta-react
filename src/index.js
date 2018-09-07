bbf85e8ec94481a2303c8be04c1812e8b0b3d55c
Marcos Cury <marcospcury@gmail.com>
2018-09-07 17:01:01 -0300

    Adding route protection

--- src/index.js
+++ src/index.js
@@ -6,13 +6,31 @@ import './css/login.css';
 import App from './App';
 import Login from './components/login';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...args }) => (
+  <Route
+    {...args}
+    render={props =>
+      (localStorage.getItem('auth-token') !== null) ? (
+        <Component {...props} />
+      ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { from: props.location }
+            }}
+          />
+        )
+    }
+  />
+);
 
 ReactDOM.render((
   <BrowserRouter>
     <div>
       <Route exact path="/" component={Login} />
-      <Route path="/timeline" component={App} />
+      <PrivateRoute path="/timeline" component={App} />
     </div>
   </BrowserRouter>
 ), document.getElementById('root'));
