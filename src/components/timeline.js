df6f83259f84ee677f8e08479da20f167997b5b9
Marcos Cury <marcospcury@gmail.com>
2018-09-08 23:18:05 -0300

    Adding timeline animation transition

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import PhotoItem from './photo/photo-item';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export default class Timeline extends React.Component {
 
   render() {
     return (
       <div className="fotos container">
-        {
-          this.props.photos.map(photo => <PhotoItem key={photo.id} photo={photo} />)
-        }
+        <ReactCSSTransitionGroup
+          transitionName="timeline"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+          {
+            this.props.photos.map(photo => <PhotoItem key={photo.id} photo={photo} />)
+          }
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
